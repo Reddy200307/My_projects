26a6a170937b75a260fe4128fbb88b920060f978
G R Prashanth Reddy <grprashanthreddy@gmail.com>
2025-12-31 21:21:56 +0530

    tried how a struct works😁

--- Embedded_uDB/src/Embedded_uDb.h
+++ Embedded_uDB/src/Embedded_uDb.h
@@ -1,4 +1,12 @@
 #ifndef EMBEDDED_H
 #define EMBEDDED_H
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+typedef struct search
+{
+        int index;
+        char link[20];
+        char keywords[10][10];
+} srch;
 #endif
--- Embedded_uDB/src/main.c
+++ Embedded_uDB/src/main.c
@@ -1,6 +1,20 @@
 #include "Embedded_uDb.h"
 int main()
 {
-        printf("hello World\n");
-        return 0;
+        srch *s;
+        s = (srch *)malloc(sizeof(srch));
+        s->index = 10;
+        strcpy(s->link, "www.google.com");
+        strcpy(s->keywords[0], "google");
+        strcpy(s->keywords[1], "search");
+        strcpy(s->keywords[2], "browser");
+        strcpy(s->keywords[3], "company");
+        printf("Printing details\n");
+        printf("link is %s index is %d\n", s->link, s->index);
+        printf("keywords\n");
+        for (int i = 0; i < 4; i++)
+        {
+                printf("%s\n", s->keywords[i]);
+        }
+        printf("\n");
 }
